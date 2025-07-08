b95ba3c125489025c70f9eb0321252b2b09629c9
Fahad Hussain <fahad.h@snoonu.com>
2025-07-08 14:31:52 +0500

    logo fix

--- src/components/HeaderSection.tsx
+++ src/components/HeaderSection.tsx
@@ -1,7 +1,7 @@
 export default function HeaderSection() {
   return (
     <div className="fixed z-20">
-      <img src="/public/logo.png" width={120} />
+      <img src="/logo.png" width={120} />
     </div>
   );
 }
